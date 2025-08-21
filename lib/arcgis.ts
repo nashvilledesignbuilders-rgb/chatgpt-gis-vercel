b153ee362a573a70c1a8aef6cb416740f357485d
nashvilledesignbuilders-rgb <nashvilledesignbuilders@gmail.com>
2025-08-20 20:56:01 -0400

    Create arcgis.ts

---
+++ lib/arcgis.ts
@@ -0,0 +1,2 @@
+export const METRO_URL = "https://maps.nashville.gov/arcgis/rest/services/MetroGov/ParcelViewer/MapServer/0";
+export const TN_URL = "https://services.tn.gov/arcgis/rest/services/Parcels/MapServer/0";
